1f3ffbecea611f5ab27c580ed3c63a8a0c4ab42d
win_yasohasegawa <dev@yasuohasegawa.com>
2018-07-09 02:15:03 +0900

    initial commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class Main : MonoBehaviour
+{
+    [DllImport("TextFileLoader")]
+    private static extern IntPtr LoadText(IntPtr name);
+
+    void Start()
+    {
+        string str1 = "C:\\Users\\yasuo\\Desktop\\WORKS_UNITY\\misc\\test.txt";
+        IntPtr ptr1 = Marshal.StringToHGlobalAnsi(str1);
+        IntPtr res = LoadText(ptr1);
+
+        Debug.Log(Marshal.PtrToStringAnsi(res));
+
+        Marshal.FreeHGlobal(ptr1);
+        Marshal.FreeHGlobal(res);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnApplicationQuit()
+    {
+
+    }
+}
